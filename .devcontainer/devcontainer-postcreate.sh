4e90b5703350d5bee34170654a155dccfececc69
Odd Stråbø <oddstr13@openshell.no>
2024-10-19 20:09:17 +0000

    Begin work on firmware

--- .devcontainer/devcontainer-postcreate.sh
+++ .devcontainer/devcontainer-postcreate.sh
@@ -10,6 +10,8 @@ arduino-cli lib update-index
 arduino-cli lib install --git-url https://github.com/oddstr13/arduino-nfc.git#b1ca71170e2984495fe6904605e87671fa3a89ce
 arduino-cli lib install Ethernet@2.0.2
 arduino-cli lib install "Electronic Cats PN7150@2.1.0"
+arduino-cli lib install ArduinoHttpClient@0.6.1
+arduino-cli lib install ArduinoJson@7.2.0
 
 arduino-cli config add board_manager.additional_urls https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json
 arduino-cli core install rp2040:rp2040@4.0.2
