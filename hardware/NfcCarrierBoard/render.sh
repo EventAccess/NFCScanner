d7798592bfe1a8dfda8b31dd2db6a2d404a962c6
Odd Stråbø <oddstr13@openshell.no>
2025-01-21 05:52:40 +0000

    Render schematic pdf Export BOM

--- hardware/NfcCarrierBoard/render.sh
+++ hardware/NfcCarrierBoard/render.sh
@@ -47,9 +47,15 @@ fi
 # Bundle into zip file for DirtyPCBs
 zip -r9 "${OUTPUT}/${PROJECT}_${DATETIME_FN}_${VERSION}.zip" "${GERBER_STAGING}"
 
+# Output KiCad linting errors
 kicad-cli sch erc --output="${OUTPUT}/schematic-erc.json" "${KICAD_DEFINES[@]}" "${PROJECT}.kicad_sch" --severity-error --severity-warning --format=json
+kicad-cli pcb drc --output="${OUTPUT}/pcb-drc.json" "${KICAD_DEFINES[@]}" "${PCB_FILE}" --severity-error --severity-warning --format=json
 # jq 'del(.sheets[].violations[]|select(.excluded==true))' "${OUTPUT}/schematic-erc.json"
 
-kicad-cli pcb drc --output="${OUTPUT}/pcb-drc.json" "${KICAD_DEFINES[@]}" "${PCB_FILE}" --severity-error --severity-warning --format=json
-
+# Render schematic
 kicad-cli sch export svg --output="${OUTPUT}" "${KICAD_DEFINES[@]}" "${PROJECT}.kicad_sch"
+kicad-cli sch export pdf --output="${OUTPUT}" "${KICAD_DEFINES[@]}" "${PROJECT}.kicad_sch"
+
+# Output BOM
+# TODO: Populate relevant schematic part fields
+kicad-cli sch export bom --output="${OUTPUT}"/${PROJECT}-BOM.csv ${PROJECT}.kicad_sch --fields 'MPN,DigiKey,${QUANTITY},Reference,Value' --labels='MPN,DigiKey,Qty,Refs,Value' --exclude-dnp
